1a08779aed3dab1579c823eccf4cebceda12d7ca
“nathinho89 <nathinho@outlook.com>
2025-08-18 16:53:02 +0100

    task: added !! to test caching rules

--- packages/core-utils/src/__tests__/greet.spec.ts
+++ packages/core-utils/src/__tests__/greet.spec.ts
@@ -3,10 +3,10 @@ import { greet } from "../index";
 
 describe("greet", () => {
   it("greets a person by name", () => {
-    expect(greet("Nathan")).toBe("Hello Nathan!");
+    expect(greet("Nathan")).toBe("Hello Nathan!!");
   });
 
   it("works with an empty string", () => {
-    expect(greet("")).toBe("Hello !");
+    expect(greet("")).toBe("Hello !!");
   });
 });
